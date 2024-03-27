08e24d2d9bd5135ccaea4027d1f53d3df5e77725
oussama <ou.charafi@gmail.com>
2024-03-27 23:03:00 +0000

    add fonction with API for get 10 cities

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
-const ArryOfCreateAccount =[];
 function change(){
-    document.getElementById("section-wrapper-account").style.display = "block"
-    let switch_log =document.getElementById("switch-login");
-    let checked = true;
-    let close = document.getElementById("close");
-    close.addEventListener("click", ()=>{
-        document.getElementById("section-wrapper-account").style.display = "none"
-    });
+  document.getElementById("section-wrapper-account").style.display = "block"
+  let switch_log =document.getElementById("switch-login");
+  let checked = true;
+  let close = document.getElementById("close");
+  close.addEventListener("click", ()=>{
+      document.getElementById("section-wrapper-account").style.display = "none"
+  });
 
-    document.querySelector(".sing").addEventListener('click',function(){
-        if(checked === true){
-            document.getElementById("gif").src ="videos/gif2.gif"
-            document.getElementById("text").innerHTML = "if you need account !, click in Create"
-            switch_log.style.transition = "0.8s"
-            switch_log.style.transform = "translateX(100%)"
-            switch_log.style.background = "#0a0a0a"
-            switch_log.style.borderRadius = "0px"
-            switch_log.style.borderTopRightRadius = "25px"
-            switch_log.style.borderBottomRightRadius = "25px"
-            this.innerHTML = "Create";
-            checked = false;
-        }else{
-            document.getElementById("text").innerHTML = "if you have account !, click in Login"
-            document.getElementById("gif").src ="videos/gif1.gif"
-            switch_log.style.transition = "0.8s"
-            switch_log.style.transform = "translateX(0%)"
-            switch_log.style.background = "#222"
-            switch_log.style.borderRadius = "0px"
-            switch_log.style.borderTopLeftRadius = "25px"
-            switch_log.style.borderBottomLeftRadius = "25px"
-            this.innerHTML = "Login";
-            checked = true;
-        }
-    });
+  document.querySelector(".sing").addEventListener('click',function(){
+      if(checked === true){
+          document.getElementById("gif").src ="videos/gif2.gif"
+          document.getElementById("text").innerHTML = "if you need account !, click in Create"
+          switch_log.style.transition = "0.8s"
+          switch_log.style.transform = "translateX(100%)"
+          switch_log.style.background = "#0a0a0a"
+          switch_log.style.borderRadius = "0px"
+          switch_log.style.borderTopRightRadius = "25px"
+          switch_log.style.borderBottomRightRadius = "25px"
+          this.innerHTML = "Create";
+          checked = false;
+      }else{
+          document.getElementById("text").innerHTML = "if you have account !, click in Login"
+          document.getElementById("gif").src ="videos/gif1.gif"
+          switch_log.style.transition = "0.8s"
+          switch_log.style.transform = "translateX(0%)"
+          switch_log.style.background = "#222"
+          switch_log.style.borderRadius = "0px"
+          switch_log.style.borderTopLeftRadius = "25px"
+          switch_log.style.borderBottomLeftRadius = "25px"
+          this.innerHTML = "Login";
+          checked = true;
+      }
+  });
 }
+const ArryOfCreateAccount =[];
+const cities = [
+  "New York",
+  "Paris",
+  "London",
+  "Tokyo", 
+  "Berlin", 
+  "Rome", 
+  "Sydney", 
+  "Moscow", 
+  "Los Angeles", 
+  "Dubai"
+];
 const apiKey = 'b4865ee9d796cdba2a29ff974402d665';
 let lat = null;
 let lon = null;
 let dataCity = [];
-
 const namecity = "agadir";
 
-const apiUrlName = `http://api.openweathermap.org/geo/1.0/direct?q=${namecity}&limit=5&appid=${apiKey}`;
-
+function one(){
+  const apiUrlName = `http://api.openweathermap.org/geo/1.0/direct?q=${namecity}&limit=5&appid=${apiKey}`;
 fetch(apiUrlName)
   .then(res => {
     return res.json();
@@ -57,9 +68,8 @@ fetch(apiUrl)
         .then(response => {
           return response.json();
         }).then(weatherData => {
-          
              dataCity = weatherData;
-          
+             console.log(dataCity);
 
         }).catch(error => {
           console.error('ُerror in return data:', error);
@@ -74,29 +84,34 @@ fetch(apiUrl)
   setTimeout(function(){
     console.log(dataCity);
  },1000)
-// const apiKey = 'b4865ee9d796cdba2a29ff974402d665'; 
-// let lat = null; 
-// let lon = null;
+}
+// one();
 
-// let namecity = "tadla"
-// const apiUrlName = `http://api.openweathermap.org/geo/1.0/direct?q=${namecity},{state code},{country code}&limit=5&appid=${apiKey}`;
-// const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+// this scetion for get 10 cities //
+async function getLonLat(name){
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${apiKey}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      const {lat , lon} = await data[0]
+      return {lat , lon};
+      
+}
+async function getCities(lat , lon){
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;    
+  const response = await fetch(apiUrl);
+      return response.json();
+}
+async function tenCities(){
+  for(const name of cities){
+    const {lat , lon} = await getLonLat(name);
+    const data = await getCities(lat , lon);
+    showAll(data);
+  }
+}
+function showAll(data){
+  console.log(data);
+}
+tenCities();
+////
 
-// fetch(apiUrl)
-//   .then(response => {
-//     return response.json();
-//   }).then(data => {
-//         console.log(data);
-//   }).catch(error => {
-//     console.error('Error de conixion', error);
-//   });
 
-// fetch(apiUrlName)
-//   .then(res => {
-//     return res.json();
-//   }).then(data => {
-//         lat = data.lat;
-//         lon = data.lon;
-//   }).catch(error => {
-//     console.error('Error de conixion', error);
-//   });
